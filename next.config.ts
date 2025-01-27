4340c606470786c3aef1e15ddf0762d8381c9f47
yuni <yuni7550@gmail.com>
2025-01-27 19:31:56 +0900

    faker

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,36 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+
+  images: {
+    domains: ["cloudflare-ipfs.com", "https://fastly.picsum.photos", "https://loremflickr.com", "https://picsum.photos"], // 허용할 도메인 목록을 지정
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cloudflare-ipfs.com",
+        port: "",
+        pathname: "/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/",
+      },
+      {
+        protocol: "https",
+        hostname: "https://fastly.picsum.photos/",
+        port: "",
+        pathname: "/id/",
+      },
+      {
+        protocol: "https",
+        hostname: "loremflickr.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/components/KeyVisual.tsx
+++ src/components/KeyVisual.tsx
@@ -1,7 +1,39 @@
-
+import { faker } from '@faker-js/faker';
+import Image from 'next/image';
 
 export default function KeyVisual() {
+
+    const photos = [
+        {
+            url: faker.image.urlPicsumPhotos({ width: 128, height: 128 }),
+            alt: faker.word.adjective(100)
+        },
+        {
+            url: faker.image.urlPicsumPhotos({ width: 128, height: 128 }),
+            alt: faker.word.adjective(100)
+        },
+        {
+            url: faker.image.urlPicsumPhotos({ width: 128, height: 128 }),
+            alt: faker.word.adjective(100)
+        },
+    ]
     return (
-        <section>key visual</section>
+        <section>
+            {
+                photos.map(photo => {
+                    return (
+                        <div>
+                            <Image
+                                src={photo.url}
+                                width={300}
+                                height={200}
+                                alt={photo.alt}
+                            />
+                        </div>
+                    )
+                })
+            }
+            
+        </section>
     )
 }
