ff2149088fff3b43fd5f8f22720b041109bd14a6
yunijo <yuni7550@gmail.com>
2025-02-06 15:02:35 +0900

    swiper

--- src/components/KeyVisual.tsx
+++ src/components/KeyVisual.tsx
@@ -1,8 +1,11 @@
 import { faker } from '@faker-js/faker';
 import Image from 'next/image';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay,Pagination } from 'swiper/modules';
 
+import 'swiper/css';
 export default function KeyVisual() {
-
+/*
     const photos = [
         {
             url: faker.image.urlPicsumPhotos({ width: 128, height: 128 }),
@@ -17,23 +20,41 @@ export default function KeyVisual() {
             alt: faker.word.adjective(100)
         },
     ]
+        */
+    const photos = [
+        {
+            url: '/1.jpg',
+            alt: '/1.jpg'
+        },
+        {
+            url: '/2.jpg',
+            alt: '/2.jpg'
+        },
+        {
+            url: '/3.jpg',
+            alt: '/3.jpg'
+        },
+    ]
     return (
-        <section>
-            {
-                photos.map(photo => {
-                    return (
-                        <div>
-                            <Image
-                                src={photo.url}
-                                width={300}
-                                height={200}
-                                alt={photo.alt}
-                            />
-                        </div>
-                    )
-                })
-            }
-            
+        <section>               
+            <Swiper
+                modules={[Autoplay,Pagination]}
+                autoplay={{
+                    delay: 4500,
+
+                }}
+            >
+                {photos.map((photo) => (
+                <SwiperSlide>
+                    <Image
+                        src={photo.url}
+                        width={300}
+                        height={200}
+                        alt={photo.alt}
+                    />
+                </SwiperSlide>
+                ))}
+            </Swiper>                  
         </section>
     )
 }
