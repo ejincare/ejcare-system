7458f9f21d5b3afc81748bc89842e0f3cd6a56c1
yunijo <yuni7550@gmail.com>
2025-03-13 15:44:08 +0900

    -

--- src/components/PhotoCarousel.tsx
+++ src/components/PhotoCarousel.tsx
@@ -8,21 +8,45 @@ export default function PhotoCarousel() {
   SwiperCore.use([Autoplay,Pagination]);
   const data = [
     {
+      name: "경기 용인시 S건물",
+      floor: "지하 2층 ~ 지상 10층",
+      area: "연면적 31,966.29m2",
+      con: "상주, 미화관리",
       src: "/1.jpg",
       alt: "Image 1",
     }, {
+      name: "경기 안성시 E건물",
+      floor: "지하 3층 ~ 지상 29층",
+      area: "연면적 69,384.00m2",
+      con: "상주, 미화관리",      
       src: "/2.jpg",
       alt: "Image 2",
     }, {
-      src: "/3.jpg",
-      alt: "Image 3",
-    }, {
+      name: "경기 여주시 S건물",
+      floor: "지하 2층 ~ 지상 20층",
+      area: "연면적 57,569.7843m2",
+      con: "상주, 미화관리",        
       src: "/1.jpg",
       alt: "Image 4",
     }, {
+      name: "경기 이천시 I건물",
+      floor: "지하 1층 ~ 지상 15층",
+      area: "연면적 59,762.577m2",
+      con: "상주, 미화관리",        
       src: "/2.jpg",
       alt: "Image 5",
     }, {
+      name: "서울시 동대문구 H건물",
+      floor: "지하 6층 ~ 지상 20층",
+      area: "연면적 50,203.2415m2",
+      con: "상주, 미화관리",        
+      src: "/3.jpg",
+      alt: "Image 6",
+    }, {
+      name: "경기 구리시 S건물",
+      floor: "지하 2층 ~ 지상 27층",
+      area: "연면적 15,472.80m2",
+      con: "상주, 미화관리",        
       src: "/3.jpg",
       alt: "Image 6",
     }
@@ -55,13 +79,23 @@ export default function PhotoCarousel() {
             >
               {data.map((item, index) => (
                 <SwiperSlide key={index}>
-                  <Image
-                    width={512}
-                    height={512}
-                    src={item.src}
-                    className="rounded-lg"
-                    alt={item.alt}
-                  />
+                  <div className="lg:h-full rounded-[10px] overflow-hidden sm:flex sm:justify-between h-[400px] lg:max-h-[332px] bg-[#f5f5f8]">
+                    <div className="relative flex-none sm:flex sm:justify-start flex-col justify-between bg-gray-05 p-6 md:pr-[30px] text-[#282828] sm:w-auto text-left">
+                      <h4 className="text-xl md:text-xl font-semibold leading-[140%] lg:p-0">{item.name}</h4>
+                      <div className="items-center justify-between lg:mt-10 lg:p-0 pt-5">
+                        <div className="text-base">{item.floor}</div>
+                        <div className="text-xs text-gray-400">({item.area})</div>
+                      </div>
+                      <div className="grid grid-cols-2 lg:grid-cols-1 mt-5 text-gray-500 text-base">{item.con}</div>
+                    </div>
+                    <Image
+                      width={512}
+                      height={512}
+                      src={item.src}
+                      className="flex-none w-full sm:w-20 w-12 h-12 sm:h-50"
+                      alt={item.alt}
+                    />
+                  </div>
                 </SwiperSlide>
               ))}
           </Swiper>
