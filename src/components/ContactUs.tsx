123d1b59413c64005da13684bd01f8f52f6195a4
yunijo <yuni7550@gmail.com>
2025-03-13 15:49:31 +0900

    -

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -2,7 +2,7 @@
 
 export default function ContactUs() {
     return (
-        <section className="min-h-48 p-4 center text-gray-800 text-2xl font-bold">
+        <section className="max-w-screen-xl mx-auto justify-center w-full min-h-48 p-4 center text-gray-800 text-2xl font-bold">
           <h4 className="">Contact us</h4>
         </section>
     )
--- src/components/Email.tsx
+++ src/components/Email.tsx
@@ -2,7 +2,7 @@
 
 export default function Email() {
     return (
-        <section className="min-h-48 p-4 center text-gray-800 text-2xl font-bold">
+        <section className="max-w-screen-xl mx-auto justify-center w-full min-h-48 p-4 center text-gray-800 text-2xl font-bold">
           <h4 className="">Send Email</h4>
         </section>
     )
