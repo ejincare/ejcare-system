c29d8708a885d10817af6944b34d3d99d6d8c973
DESKTOP-6PNLAUN\Hyuk <yng826@gmail.com>
2025-03-21 21:18:55 +0900

    static site

--- next.config.ts
+++ next.config.ts
@@ -3,35 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
-  output: 'export',
+  output: 'export',  
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cloudflare-ipfs.com",
-        port: "",
-        pathname: "/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/",
-      },
-      {
-        protocol: "https",
-        hostname: "https://fastly.picsum.photos/",
-        port: "",
-        pathname: "/id/",
-      },
-      {
-        protocol: "https",
-        hostname: "loremflickr.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "picsum.photos",
-        port: "",
-        pathname: "/**",
-      },
-    ],
+    unoptimized: true,
   },
 };
+
 
 export default nextConfig;
