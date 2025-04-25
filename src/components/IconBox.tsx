6d7994601cc26191bf68e52499cc6265e3773cea
DESKTOP-6PNLAUN\Hyuk <yng826@gmail.com>
2025-04-25 09:48:38 +0900

    이미지 해상도도

--- src/components/IconBox.tsx
+++ src/components/IconBox.tsx
@@ -13,7 +13,7 @@ export default function IconBox({key, iconPath, title, description}: IconBoxProp
         <li className={`${
             key%2 == 0 ? 'sm:translate-y-[-120px]' : 'translate-y-[0px]'
         }`}>         
-            <Image src={iconPath} alt="icon" className="flex-none w-full" width={48} height={48} />
+            <Image src={iconPath} alt="icon" className="flex-none w-full" width={290} height={387} />
             <div className="text-[15px] sm:text-[19px] block leading-[1.3] mt-3">{title}</div>
             <div className="text-[13px] font-light leading-[1.6667em] mt-3">{description}</div>
         </li>
