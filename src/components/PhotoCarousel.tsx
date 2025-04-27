78b7af58fd5b138b5b12038fbd1ac8e6d959d9de
yuni <yuni7550@gmail.com>
2025-04-27 16:24:57 +0900

    graphql

--- src/components/PhotoCarousel.tsx
+++ src/components/PhotoCarousel.tsx
@@ -2,78 +2,154 @@ import Image from "next/image";
 import SwiperCore from "swiper";
 import { Autoplay, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { useQuery, gql } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+
+// const client = new ApolloClient({
+//     uri: "https://admin.ejincare.com/",
+//     cache: new InMemoryCache(),
+// });
+import { loadErrorMessages, loadDevMessages } from "@apollo/client/dev";
+
+if (1) {
+  // Adds messages only in a dev environment
+  loadDevMessages();
+  loadErrorMessages();
+}
 
 export default function PhotoCarousel() {
   SwiperCore.use([Autoplay,Pagination]);
-  const data = [
-    {
-      name: "경기 용인시 S건물",
-      floor: "지하 2층 ~ 지상 10층",
-      area: "연면적 31,966.29m2",
-      con: "상주, 미화관리",
-      src: "/signaturegwanggyo.jpg",
-      alt: "signaturegwanggyo",
-    }, {
-      name: "경기 안성시 E건물",
-      floor: "지하 3층 ~ 지상 29층",
-      area: "연면적 69,384.00m2",
-      con: "상주, 미화관리",      
-      src: "/e-annsung.png",
-      alt: "e-annsung",
-    }, {
-      name: "경기 여주시 S건물",
-      floor: "지하 2층 ~ 지상 20층",
-      area: "연면적 57,569.7843m2",
-      con: "상주, 미화관리",        
-      src: "/yeojucenterville.png",
-      alt: "yeojucenterville",
-    }, {
-      name: "경기 이천시 I건물",
-      floor: "지하 1층 ~ 지상 15층",
-      area: "연면적 59,762.577m2",
-      con: "상주, 미화관리",        
-      src: "/lanfastiam.png",
-      alt: "lanfastiam",
-    }, {
-      name: "서울시 동대문구 H건물",
-      floor: "지하 6층 ~ 지상 20층",
-      area: "연면적 50,203.2415m2",
-      con: "상주, 미화관리",        
-      src: "/hillstatejangan.png",
-      alt: "hillstatejangan",
-    }, {
-      name: "경기 구리시 S건물",
-      floor: "지하 2층 ~ 지상 27층",
-      area: "연면적 15,472.80m2",
-      con: "상주, 미화관리",        
-      src: "/guriterrace.jpg",
-      alt: "guriterrace",
-    }, {
-      name: "경기 부천시 H건물",
-      floor: "지하 5층 ~ 지상 29층",
-      area: "연면적 25,676.98m2",
-      con: "상주, 미화관리",        
-      src: "/bucheonhyundai.png",
-      alt: "bucheonhyundai",
-    }, {
-      name: "경기 시흥시 H건물",
-      floor: "지하 5층 ~ 지상 10층",
-      area: "연면적 17,106.5375m2",
-      con: "상주, 미화관리",        
-      src: "/hillstatesiheung.jpg",
-      alt: "hillstatesiheung",
-    }, {
-      name: "경기 의정부시 H건물",
-      floor: "지하 2층 ~ 지상 15층",
-      area: "연면적 1,123.77m2",
-      con: "상주, 미화관리",        
-      src: "/passiblestation.png",
-      alt: "passiblestation",
+  // const data = [ 
+  //   {
+  //     name: "경기 용인시 S건물",
+  //     floor: "지하 2층 ~ 지상 10층",
+  //     area: "연면적 31,966.29m2",
+  //     con: "상주, 미화관리",
+  //     src: "/signaturegwanggyo.jpg",
+  //     alt: "signaturegwanggyo",
+  //   }, {
+  //     name: "경기 안성시 E건물",
+  //     floor: "지하 3층 ~ 지상 29층",
+  //     area: "연면적 69,384.00m2",
+  //     con: "상주, 미화관리",      
+  //     src: "/e-annsung.png",
+  //     alt: "e-annsung",
+  //   }, {
+  //     name: "경기 여주시 S건물",
+  //     floor: "지하 2층 ~ 지상 20층",
+  //     area: "연면적 57,569.7843m2",
+  //     con: "상주, 미화관리",        
+  //     src: "/yeojucenterville.png",
+  //     alt: "yeojucenterville",
+  //   }, {
+  //     name: "경기 이천시 I건물",
+  //     floor: "지하 1층 ~ 지상 15층",
+  //     area: "연면적 59,762.577m2",
+  //     con: "상주, 미화관리",        
+  //     src: "/lanfastiam.png",
+  //     alt: "lanfastiam",
+  //   }, {
+  //     name: "서울시 동대문구 H건물",
+  //     floor: "지하 6층 ~ 지상 20층",
+  //     area: "연면적 50,203.2415m2",
+  //     con: "상주, 미화관리",        
+  //     src: "/hillstatejangan.png",
+  //     alt: "hillstatejangan",
+  //   }, {
+  //     name: "경기 구리시 S건물",
+  //     floor: "지하 2층 ~ 지상 27층",
+  //     area: "연면적 15,472.80m2",
+  //     con: "상주, 미화관리",        
+  //     src: "/guriterrace.jpg",
+  //     alt: "guriterrace",
+  //   }, {
+  //     name: "경기 부천시 H건물",
+  //     floor: "지하 5층 ~ 지상 29층",
+  //     area: "연면적 25,676.98m2",
+  //     con: "상주, 미화관리",        
+  //     src: "/bucheonhyundai.png",
+  //     alt: "bucheonhyundai",
+  //   }, {
+  //     name: "경기 시흥시 H건물",
+  //     floor: "지하 5층 ~ 지상 10층",
+  //     area: "연면적 17,106.5375m2",
+  //     con: "상주, 미화관리",        
+  //     src: "/hillstatesiheung.jpg",
+  //     alt: "hillstatesiheung",
+  //   }, {
+  //     name: "경기 의정부시 H건물",
+  //     floor: "지하 2층 ~ 지상 15층",
+  //     area: "연면적 1,123.77m2",
+  //     con: "상주, 미화관리",        
+  //     src: "/passiblestation.png",
+  //     alt: "passiblestation",
+  //   }
+  // ]
+  interface Portfolio {
+    id: string;
+    title: string;
+    portfolioImage: {
+      "sourceUrl": string,
+      "mediaItemUrl": string,
+      "filePath": string,
+      "fileSize": number,
+      "altText": string
     }
-  ]
+  }
+  
+  interface PortfoliosData {
+    portfolios: Portfolio[];
+  }
+  
+  const GET_PORTFOLIOS = gql`
+    query PortfolioQuery {
+      portfolios {
+        edges {
+          node {
+            id
+            title
+            portfolioArea
+            portfolioCon
+            portfolioFloor
+            portfolioImage {
+              node {
+                altText
+                filePath
+                fileSize
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const { data, loading, error } = useQuery<PortfoliosData>(GET_PORTFOLIOS);
+
+  if (loading) {
+    return <h2>로딩중</h2>;
+  }
+
+  if (error) {
+    return <h1>에러 발생</h1>;
+  }
+
+  const portfolios = data?.portfolios.slice(0, 4);
+
+  if (!portfolios) {
+    return <h1>포트폴리오가 없습니다.</h1>;
+  }
+
+  if (portfolios) {
+    return <h1>포트폴리오가 있습니다.</h1>;
+    
+  }
+
     return (
         <section className="max-w-screen-xl mx-auto justify-center w-full text-center">
+
+        
           <Swiper
                 modules={[Autoplay,Pagination]}
                 loop={true} // Enable infinite looping
@@ -99,30 +175,31 @@ export default function PhotoCarousel() {
                 }}
                 
             >
-              {data.map((item, index) => (
-                <SwiperSlide key={index} className="!h-[350px] sm:!h-[300px]">
+              {data?.portfolios?.map((portfolio: Portfolio, idx: number) => (
+                <SwiperSlide key={`country-${idx}`} className="!h-[350px] sm:!h-[300px]">
                   <div className="lg:h-full rounded-[10px] overflow-hidden sm:flex sm:justify-between h-[350px] sm:!h-[300px] bg-[#282828]">
                     <div className="relative flex-none sm:flex sm:justify-start flex-col justify-between bg-gray-05 p-6 md:pr-[30px] sm:w-auto text-left">
-                      <h4 className="text-xl md:text-xl font-semibold leading-[140%] lg:p-0 text-[#e68600]">{item.name}</h4>
+                      <h4 className="text-xl md:text-xl font-semibold leading-[140%] lg:p-0 text-[#e68600]">{portfolio.title}</h4>
                       <div className="items-center justify-between lg:mt-10 lg:p-0 sm:pt-5">
-                        <div className="text-base text-[#d1d1d6]">{item.floor}</div>
-                        <div className="text-xs text-gray-400">({item.area})</div>
+                        <div className="text-base text-[#d1d1d6]"></div>
+                        <div className="text-xs text-gray-400"></div>
                       </div>
-                      <div className="grid grid-cols-2 lg:grid-cols-1 mt-5 text-[#d1d1d6] text-base">{item.con}</div>
+                      <div className="grid grid-cols-2 lg:grid-cols-1 mt-5 text-[#d1d1d6] text-base"></div>
                     </div>
                     <div>
                       <Image
                         width={512}
                         height={512}
-                        src={item.src}
+                        src={portfolio.portfolioImage.sourceUrl}
                         className="flex-none w-full sm:w-20 w-12 h-12 sm:h-50"
-                        alt={item.alt}
+                        alt={portfolio.portfolioImage.altText}
                       />
                     </div>
                   </div>
                 </SwiperSlide>
-              ))}
-          </Swiper>
+               ))}
+          </Swiper>           
+         
         </section>
     )
 }
--- src/pages/inquiry.tsx
+++ src/pages/inquiry.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import SubHeader from "@/components/SubHeader";
 import { useState } from "react";
+import axios from 'axios';
 
 interface IFormProps {
   userName: string | undefined;
@@ -34,6 +35,27 @@ export default function Inquiry() {
     setForm(nextInputs);
   }
 
+  async function inquiry_postData() {
+    try {
+      //응답 성공 
+      const response = await axios.post('https://admin.ejincare.com/wp-json/wp/v2/inquiry',{
+          //보내고자 하는 데이터 
+          user_name: form.userName,//
+          user_phone: form.phoneNumber,
+          user_email: form.email,
+          user_company: form.companyName,
+          title: form.title,
+          user_content: form.detail,
+          user_ref: form.funnel
+      });
+      
+      console.log(response);
+    } catch (error) {
+      //응답 실패
+      console.error(error);
+    }
+  }
+
   return (
     <div>
       <Header pageName={"subMenu"} />
@@ -46,7 +68,7 @@ export default function Inquiry() {
               <label htmlFor="userName" className="font-medium leading-[120%] text-[17px] text-[#131f35] w-[120px] xl:w-[140px] py-[10px] mr-[10px] text-left">이름<span className="text-[#e68600] ml-[6px]">*</span>
               </label>
               <div className="flex flex-col gap-2 md:max-w-[580px] flex-1 w-full text-[17px]">
-                <input id="userName" name="userName" type="text" className="h-[52px] focus:outline-none focus:border-2 focus:border-si-gray-6 w-full leading-[120%] 900:max-w-[580px] border border-solid border-si-gray-3 rounded-[5px] p-4 placeholder:text-si-gray-6" placeholder="이름을 입력해 주세요." value={form.userName} onChange={onChangeValue} />
+                <input id="userName" name="userName" type="text" className="h-[52px] focus:outline-none focus:border-2 focus:border-si-gray-6 w-full leading-[120%] 900:max-w-[580px] border border-solid border-si-gray-3 rounded-[5px] p-4 placeholder:text-si-gray-6 text-black" placeholder="이름을 입력해 주세요." value={form.userName} onChange={onChangeValue} />
               </div>
             </div>
             <div className="flex justify-between w-full md:max-w-[580px] ml-auto"></div>
@@ -56,7 +78,7 @@ export default function Inquiry() {
               <label htmlFor="phoneNumber" className="font-medium leading-[120%] text-[17px] text-[#131f35] w-[120px] xl:w-[140px] py-[10px] mr-[10px] text-left">연락처<span className="text-[#e68600] ml-[6px]">*</span>
               </label>
               <div className="flex flex-col gap-2 md:max-w-[580px] flex-1 w-full text-[17px]">
-                <input id="phoneNumber" name="phoneNumber" type="text" className="h-[52px] focus:outline-none focus:border-2 focus:border-si-gray-6 w-full leading-[120%] 900:max-w-[580px] border border-solid border-si-gray-3 rounded-[5px] p-4 placeholder:text-si-gray-6" placeholder="“-” 없이 숫자만 입력해 주세요." value={form.phoneNumber} onChange={onChangeValue} /></div>
+                <input id="phoneNumber" name="phoneNumber" type="text" className="h-[52px] focus:outline-none focus:border-2 focus:border-si-gray-6 w-full leading-[120%] 900:max-w-[580px] border border-solid border-si-gray-3 rounded-[5px] p-4 placeholder:text-si-gray-6 text-black" placeholder="“-” 없이 숫자만 입력해 주세요." value={form.phoneNumber} onChange={onChangeValue} /></div>
             </div>
             <div className="flex justify-between w-full md:max-w-[580px] ml-auto"></div>
           </div>
@@ -65,7 +87,7 @@ export default function Inquiry() {
               <label htmlFor="email" className="font-medium leading-[120%] text-[17px] text-[#131f35] w-[120px] xl:w-[140px] py-[10px] mr-[10px] text-left">이메일<span className="text-[#e68600] ml-[6px]">*</span>
               </label>
               <div className="flex flex-col gap-2 md:max-w-[580px] flex-1 w-full text-[17px]">
-                <input id="email" name="email" type="text" className="h-[52px] focus:outline-none focus:border-2 focus:border-si-gray-6 w-full leading-[120%] 900:max-w-[580px] border border-solid border-si-gray-3 rounded-[5px] p-4 placeholder:text-si-gray-6" placeholder="이메일 주소를 입력해 주세요." value={form.email} onChange={onChangeValue} /></div>
+                <input id="email" name="email" type="text" className="h-[52px] focus:outline-none focus:border-2 focus:border-si-gray-6 w-full leading-[120%] 900:max-w-[580px] border border-solid border-si-gray-3 rounded-[5px] p-4 placeholder:text-si-gray-6 text-black" placeholder="이메일 주소를 입력해 주세요." value={form.email} onChange={onChangeValue} /></div>
             </div>
             <div className="flex justify-between w-full md:max-w-[580px] ml-auto"></div>
           </div>
@@ -74,7 +96,7 @@ export default function Inquiry() {
               <label htmlFor="companyName" className="font-medium leading-[120%] text-[17px] text-[#131f35] w-[120px] xl:w-[140px] py-[10px] mr-[10px] text-left">회사명<span className="text-[#e68600] ml-[6px]">*</span>
               </label>
               <div className="flex flex-col gap-2 md:max-w-[580px] flex-1 w-full text-[17px]">
-                <input id="companyName" name="companyName" type="text" className="h-[52px] focus:outline-none focus:border-2 focus:border-si-gray-6 w-full leading-[120%] 900:max-w-[580px] border border-solid border-si-gray-3 rounded-[5px] p-4 placeholder:text-si-gray-6" placeholder="회사명을 입력해 주세요." value={form.companyName} onChange={onChangeValue} /></div>
+                <input id="companyName" name="companyName" type="text" className="h-[52px] focus:outline-none focus:border-2 focus:border-si-gray-6 w-full leading-[120%] 900:max-w-[580px] border border-solid border-si-gray-3 rounded-[5px] p-4 placeholder:text-si-gray-6 text-black" placeholder="회사명을 입력해 주세요." value={form.companyName} onChange={onChangeValue} /></div>
             </div>
             <div className="flex justiTfy-between w-full md:max-w-[580px] ml-auto"></div>
           </div>
@@ -82,7 +104,7 @@ export default function Inquiry() {
             <div className="flex flex-col items-start justify-between w-full md:flex-row md:items-center ">
               <label htmlFor="title" className="font-medium leading-[120%] text-[17px] text-[#131f35] w-[120px] xl:w-[140px] py-[10px] mr-[10px] text-left">제목<span className="text-[#e68600] ml-[6px]">*</span></label>
               <div className="flex flex-col gap-2 md:max-w-[580px] flex-1 w-full text-[17px]">
-                <input id="title" name="title" type="text" className="h-[52px] focus:outline-none focus:border-2 focus:border-si-gray-6 w-full leading-[120%] 900:max-w-[580px] border border-solid border-si-gray-3 rounded-[5px] p-4 placeholder:text-si-gray-6" placeholder="제목을 입력해 주세요." value={form.title} onChange={onChangeValue} /></div>
+                <input id="title" name="title" type="text" className="h-[52px] focus:outline-none focus:border-2 focus:border-si-gray-6 w-full leading-[120%] 900:max-w-[580px] border border-solid border-si-gray-3 rounded-[5px] p-4 placeholder:text-si-gray-6 text-black" placeholder="제목을 입력해 주세요." value={form.title} onChange={onChangeValue} /></div>
             </div>
             <div className="flex justify-between w-full md:max-w-[580px] ml-auto"></div>
           </div>
@@ -91,7 +113,7 @@ export default function Inquiry() {
               <label htmlFor="detail" className="font-medium leading-[120%] text-[17px] text-[#131f35] w-[100px] xl:w-[140px] py-0 md:py-[10px] text-left">내용<span className="text-[#e68600] ml-[6px]">*</span>
               </label>
               <div className="flex flex-col gap-2 flex-1 md:max-w-[580px] w-full">
-                <textarea id="detail" rows={10} name="detail" className="focus:outline-none focus:border-2 focus:border-si-gray-6 w-full md:max-w-[580px] border border-solid border-si-gray-3 rounded-[5px] p-4 placeholder:text-si-gray-6" placeholder={'- 문의 서비스 내용 - 최대 :- 지역 :- 요청 및 문의 :'} onChange={onChangeValue} ></textarea>
+                <textarea id="detail" rows={10} name="detail" className="focus:outline-none focus:border-2 focus:border-si-gray-6 w-full md:max-w-[580px] border border-solid border-si-gray-3 rounded-[5px] p-4 placeholder:text-si-gray-6 text-black" placeholder={'- 문의 서비스 내용 - 최대 :- 지역 :- 요청 및 문의 :'} onChange={onChangeValue} ></textarea>
               </div>
             </div>
           </div>
@@ -100,7 +122,7 @@ export default function Inquiry() {
               <label htmlFor="funnel" className="font-medium leading-[120%] text-[17px] shrink-0 text-[#131f35] w-[100px] xl:w-[140px] py-[10px] md:mr-[10px] text-left">유입경로 <span className="text-[#e68600] font-medium text-[17px]">*</span>
               </label>
               <div className="flex items-center relative h-fit w-full md:max-w-[580px] ">
-                <select id="funnel" name="funnel" className="focus:outline-none focus:border-2 focus:border-si-gray-6 h-[52px] invalid:border-si-main-red border border-solid w-full border-si-gray-3 rounded-[5px] px-4 bg-white text-[17px] text-si-gray-6 md:text-si-primary-text appearance-none" onChange={onChangeValue}>
+                <select id="funnel" name="funnel" className="focus:outline-none focus:border-2 focus:border-si-gray-6 h-[52px] invalid:border-si-main-red border border-solid w-full border-si-gray-3 rounded-[5px] px-4 bg-white text-[17px] text-si-gray-6 md:text-si-primary-text appearance-none text-black" onChange={onChangeValue}>
                   <option value="">선택</option>
                   <option value="광고">광고</option>
                   <option value="도서">도서</option>
@@ -114,7 +136,7 @@ export default function Inquiry() {
               </div>
             </div>
             <div className="flex justify-end w-full md:max-w-[580px] ml-auto mt-4">
-              <button className="right-0 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">제출</button>
+              <button className="right-0 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onClick={inquiry_postData}>제출</button>
             </div>
           </div>
         </div>
