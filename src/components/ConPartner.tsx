a950d1817b8da24c50b202ad0b26aa1fa99a8382
yunijo <yuni7550@gmail.com>
2025-05-12 21:03:15 +0900

    간격격

--- src/components/ConPartner.tsx
+++ src/components/ConPartner.tsx
@@ -4,9 +4,9 @@ import Image from 'next/image';
 
 export default function ConPartner() {
     return (
-      <div className='bg-[#282828] ml-[-1.25rem] mr-[-1.25rem] px-[1.25rem] py-[70px] sm:py-[200px]'>
+      <div className='bg-[#282828] ml-[-1.25rem] mr-[-1.25rem] px-[1.25rem] py-[70px] sm:py-[8rem]'>
         <section className="relative w-full mx-auto min-h-48 center text-gray-800 text-2xl font-bold">
-          <div className="css-1c2fuzs">
+          <div className="css-1c2fuzs sm:px-5">
             <div className="relative w-full mx-auto flex flex-col text-si-primary-text max-w-[1500px] w-full text-[#ffffff]">
               <h4 className="text-[28px] md:text-[56px] font-bold leading-[140%] mb-5 text-left ">반 발 앞서 준비하고, 먼저 고민합니다</h4>
               <p className="text-[16px] md:text-[21px] font-medium leading-[155%] mb-5 text-left">
