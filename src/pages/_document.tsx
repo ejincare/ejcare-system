84a2ac1a10c5b25401123bea880ef30d79dcd2a3
DESKTOP-6PNLAUN\Hyuk <yng826@gmail.com>
2025-05-17 23:44:57 +0900

    fix: gtm 위치이동 doc->app

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import { SpeedInsights } from "@vercel/speed-insights/next"
-import { GoogleTagManager } from '@next/third-parties/google'
 
 export default function Document() {
   return (
     <Html lang="ko">
       <Head />
       <body className="antialiased">
-        <GoogleTagManager gtmId="GTM-PVSPRVBH"  />
         <Main />
         <NextScript />
         <SpeedInsights />
