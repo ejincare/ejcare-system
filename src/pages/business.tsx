24ed78464827ba10e0ed0090dc196ff3dd93d789
yuni <yuni7550@gmail.com>
2025-06-07 10:24:22 +0900

    사업실적 추가가

--- src/pages/business.tsx
+++ src/pages/business.tsx
@@ -1,3 +1,4 @@
+import BusinessIconBoxWrap from "@/components/BusinessIconBoxWrap";
 import Layout from "@/components/Layout";
 import SubHeader from "@/components/SubHeader";
 import Image from "next/image";
@@ -8,6 +9,7 @@ export default function business() {
       <main className="max-w-screen-xl mx-auto justify-center w-full text-center box-border px-5 xl:px-0 pt-[72px] pb-[100px]">
         <SubHeader title_kor={"사업분야"} title_eng={"Business"}/>
 
+        <BusinessIconBoxWrap/>
         <div className="flex flex-col items-center justify-center">
           <Image src="/IMG_4636.png" alt="company" width={1000} height={1000} className="mx-auto mt-[100px]"/>
           <Image src="/IMG_4637.png" alt="company" width={1000} height={1000} className="mx-auto mt-[100px]"/>
