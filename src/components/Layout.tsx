dd74933dd35e3ae78ce567507edc219c33c6111f
yunijo <yuni7550@gmail.com>
2025-08-14 10:15:50 +0900

    타이틀변경

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -41,19 +41,19 @@ const Layout: React.FC<LayoutProps> = ({ children, pageTitle, pageDescription })
       className={`${geistSans.variable} ${geistMono.variable} font-[family-name:var(--font-geist-sans)]`}
     >
       <NextSeo
-        title = {pageTitle ?? "이진케어시스템 홈페이지 - 건물을 사랑하는 기업 ㈜이진케어시스템이 함께합니다"}
+        title = {pageTitle ?? "이진케어시스템 홈페이지 - 한국을 주도하는 아파트 관리 ㈜이진케어시스템이 함께합니다"}
         description = {pageDescription ?? "이진케어시스템은 최첨단 미래형 관리 기술을 도입, 변화하는 생활환경에 빠르게 대응하고 고객의 기대에 앞서가는 새로운 환경을 조성함으로써 고객만족은 물론, 사람과 환경, 기술의 이상적인 조화를 추구, 쾌적하고 안전하며 효율적인 공간 관리를 실현하고 있습니다."}
         canonical="https://ejincare.com/"
         openGraph={{
           url: 'https://ejincare.com/',
           title: '이진케어시스템 홈페이지',
-          description: '건물을 사랑하는 기업 ㈜이진케어시스템이 함께합니다',
+          description: '한국을 주도하는 아파트 관리 ㈜이진케어시스템이 함께합니다',
           images: [
             {
               url: 'https://ejincare.com/img/ejincare-logo-1200.jpg',
               width: 1200,
               height: 630,
-              alt: '이진케어시스템 - 건물을 사랑하는 기업 ㈜이진케어시스템이 함께합니다',
+              alt: '이진케어시스템 - 한국을 주도하는 아파트 관리 ㈜이진케어시스템이 함께합니다',
               type: 'image/jpg',
             },
           ],
